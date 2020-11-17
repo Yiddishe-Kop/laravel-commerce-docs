5ab144a968cfadf977d3b22972de21c5b752a021
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-11-17 21:01:31 +0200

    style: upgrade code style

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,20 @@ module.exports = {
           css: {
             'pre code': {
               fontFamily: 'Cascadia'
-            }
+            },
+            code: {
+              fontFamily: 'Cascadia',
+              padding: '0.2em 0.5em',
+              borderRadius: theme('borderRadius.md'),
+              color: theme('colors.gray.100'),
+              backgroundColor: theme('colors.gray.800'),
+            },
+            'code::before': {
+              content: ''
+            },
+            'code::after': {
+              content: ''
+            },
           }
         },
       })
